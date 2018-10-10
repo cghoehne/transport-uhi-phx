1873d77facbc08714310d0bac836813c7a982200
Chris Hoehne <31117250+cghoehne@users.noreply.github.com>
2018-10-10 15:18:59 -0700

    scrapped variable buffer of OSM data in R because of time and functionality issues. instead, reverted OSM data prep to a script that outputs necessary formatted OSM data from raw OSM road network data to push through QGIS variable buffer. This initial OSM script will be kept for transparency but the variable buffer of OSM data won't be automated (simple but timeconsuming data prep task), instaed the raw input and prepped output will be provided. may include a python script for this task in the future.

--- 1b-spatial-data-prep.R
+++ 1b-spatial-data-prep.R
@@ -24,24 +24,11 @@ if(length(new.packages)) install.packages(new.packages)
 # load packages
 lapply(list.of.packages, library, character.only = TRUE)
 
-
-#-#-#-#-#-#-#-#
-# Import data #
-#-#-#-#-#-#-#-#
-
 # import cleaned station data
 w.stations <- readRDS(here("data/station-data.rds"))
 
-# import osm data (maricopa county clipped raw road network data)
-osm <- shapefile(here("data/shapefiles/osm/maricopa_county_osm_roads.shp")) # city labels shpfile
-
 # import uza boundary
 uza.border <- shapefile(here("data/shapefiles/boundaries/maricopa_county_uza.shp")) # uza shpfile
-
-
-#-#-#-#-#-#-#
-# Prep data #
-#-#-#-#-#-#-#
 
 # convert stations data.table w/ lat-lon to coordinates (SpatialPointDataFrame)
 w.stations.spdf <- SpatialPointsDataFrame(coords = w.stations[, .(lon,lat)], data = w.stations,
@@ -50,8 +37,11 @@ w.stations.spdf <- SpatialPointsDataFrame(coords = w.stations[, .(lon,lat)], dat
 # transform stations crs to same as other shapefiles 
 w.stations.spdf <- spTransform(w.stations.spdf, crs(uza.border))
 
-# clip station points to ones w/in uza 
-uza.stations <- raster::intersect(w.stations.spdf, uza.border)
+# buffer uza boundary by 1 mile
+uza.buffer <- gBuffer(uza.border, byid = F, width = 5280)
+
+# clip station points to ones w/in uza 1mi buffer
+uza.stations <- raster::intersect(w.stations.spdf, uza.buffer)
 
 # filter station.list to uza stations
 uza.stations.list <- w.stations[station.name %in% uza.stations$station.name]
@@ -60,86 +50,15 @@ uza.stations.list <- w.stations[station.name %in% uza.stations$station.name]
 weather.data <- readRDS(here("data/2017-weather-data.rds"))
 uza.weather <- weather.data[station.name %in% uza.stations.list$station.name]
 
-# store table of roadway classes as data.table, rename V1 to fclass
-road.classes <- as.data.table(table(osm@data$fclass))
-setnames(road.classes, "V1", "fclass")
-
-# create id column for fclasses where cars are unsuitable or prohibited. assume 'unknown' is unsuitable without info to assume otherwise
-# open the data dic to see details on this (page 15) by running in console: getOption('viewer')(here('data/shapefiles/osm/osm-data-dictionary.pdf'))
-road.classes[,auto.use := "Y"]
-road.classes[fclass %in% c("bridleway","cycleway","footway","path","steps", "pedestrian", "unknown"), auto.use := "N"]
-
-# also id fclasses of 'track' as 'P' for partial use/suitablity. data dic: "For agricultural use, in forests, etc. Often gravel roads."
-road.classes[fclass %in% c("track","track_grade1","track_grade2","track_grade3","track_grade4","track_grade5"), auto.use := "P"]
-
-# **Curently, OSM data in Phoenix does not have lane data. in light of this:
-# assign roadway widths based on roadway class
-# assume number of lanes is constant by class
-# assume outside shoulder width is 10 ft
-# assume inside shoulde width is 10 ft and only exists for highway/express roadway classes
-# assume lane width is 12 ft
-# 1 ft = 0.3048 meters
-
-# import additional fclass info w/ assumptions of 1 way lanes keyed .csv
-fclass.info <- fread(here("data/shapefiles/osm/fclass_info.csv"))
-road.classes <- merge(road.classes, fclass.info, by = "fclass")
-
-# store osm data for quick calcs of new variables and rebind
-osm.dt <- as.data.table(osm@data)
-
-# merge fclass.info with osm data
-osm.dt <- merge(osm.dt, road.classes, by = "fclass")
-
-# calulate the width of the road based on the number of lanes, 
-# if it is 1 or 2way, and the shoulder widths
-osm.dt$road.width.m <- ifelse(osm.dt$oneway == "B", # if the road has lanes in each direction
-                              (osm.dt$lanes.1way * 2 * 12 * 0.3048) # width (m) = # lns/dir * 2 dir * 12 ft/ln * 0.3048 m/ft
-                              + (osm.dt$in.shldr + osm.dt$out.shldr) * 0.3048, # in & out shoulder width (ft) * 0.3048 m/ft
-                              (osm.dt$lanes.1way * 1 * 12 * 0.3048)
-                              + (osm.dt$in.shldr + osm.dt$out.shldr) * 0.3048) # in & out shoulder width (ft) * 0.3048 m/ft) # same but in 1 dir
-
-# update new relevant vars so they appear in osm@data
-osm <- merge(osm, osm.dt[, .(osm_id,auto.use,road.width.m,descrip)], by = "osm_id")
-
-# buffer uza boundary by ~1 mile, then clip osm network by buffered uza 
-uza.buffer <- gBuffer(uza.border, byid = F, width = 5280, capStyle = "FLAT")
-
-# transform osm crs to EPSG:2223
-osm <- spTransform(osm, crs(uza.buffer))
-
-# clip osm data to the buffered uza
-osm.uza <- raster::intersect(osm, uza.buffer)
-
-# buffer links by half of roadway width by loop for each roadway class type
-w <- unique(osm.uza$road.width.m) # list of unique roadway widths to buffer by
-w <- w[!is.na(w) & w > 0] # remove NA and 0 width roads
-b <- list() # empty list for foreach
+# foreach loop in parallel to buffer stations points by multiple radii
+# note that the foreach loop in parallel probably isn't necessary
 my.cores <- parallel::detectCores()  # store computers cores
 registerDoParallel(cores = my.cores) # register parallel backend
-
-# foreach loop in parallel to buffer links
-# (stores as list of SpatialPointDataFrames)
-#*** WARNING: multiple hour run time for these next few lines ***
-osm.links.buffered <- foreach(i = 1:length(w), .packages = c("sp","rgeos")) %dopar% {
-  x <- osm.uza[osm.uza$road.width.m == w[i], ]
-  b[[i]] <- gBuffer(x, byid = F, width = w[i] / 2, capStyle = "FLAT")
-}
-
-# also buffer stations points by multiple radii
 radii.buffers <- c(50,100,200,500,1000,2500) # radii for buffer on each station point
-
-# foreach loop in parallel to buffer points by variable distances
-# (stores as list of SpatialPointDataFrames)
 stations.buffered <- foreach(i = 1:length(radii.buffers), .packages = c("sp","rgeos"), .combine = c) %dopar% {
-  gBuffer(uza.stations, byid = T, width = radii.buffers[i]) }
-
-
-#-#-#-#-#-#-#-#
-# Export data #
-#-#-#-#-#-#-#-#
+  gBuffer(uza.stations, byid = T, width = radii.buffers[i]) } # (stores as list of SpatialPointDataFrames)
 
 # save formatted spatial data for analysis scripts 
-saveRDS(osm.links.buffered, here("data/osm-links-buffers-sp-list.rds")) # saves buffered osm  data as list of spatial r objects
 saveRDS(stations.buffered, here("data/station-buffers-sp-list.rds")) # saves buffered station data as list of spatial r objects
 saveRDS(uza.stations, here("data/uza-station-data.rds")) # saves station data as spatial r object (points)
 saveRDS(uza.weather, here("data/2017-uza-weather-data.rds")) # saves weather data filtered to only uza stations
