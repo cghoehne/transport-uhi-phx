9c4ac34349810d9b128b3d77ea4f3646171639d6
Chris Hoehne <31117250+cghoehne@users.noreply.github.com>
2019-03-21 16:22:58 -0700

    added script to contain final summary statistics for manuscript

--- R/7-summary-stats-manuscript.R
+++ R/7-summary-stats-manuscript.R
@@ -14,12 +14,16 @@ weather.raw[, mean(winspd, na.rm = T), by = hour(date.time)][order(hour)]
 weather.raw[, quantile(winspd, probs = 0.5, na.rm = T), by = hour(date.time)][order(hour)]
 weather.raw[, quantile(winspd, probs = 0.5, na.rm = T)]
 
+weather.raw[, mean(temp.c, na.rm = T), by = hour(date.time)][order(hour)]
+
 
 # PAVEMENT SURFACE
 # import data
 folder <- paste0(here("data/outputs"),"/run_metadata_20190320_192039/")
 all.surface.data <- readRDS(paste0(folder, "all_pave_surface_data.rds"))
 colnames(all.surface.data)
+
+all.surface.data[, daytime := factor(ifelse(hour(date.time) %in% c(7:18), "Day", "Night"), levels = c("Day","Night"))]
 
 # flux comparisons by type
 all.surface.data[, mean(q.rad + q.cnv), by = c("pave.name", "batch.name", "albedo")][order(V1)]
@@ -28,5 +32,12 @@ all.surface.data[, mean(q.rad + q.cnv), by = c("pave.name", "batch.name", "albed
 all.surface.data[, quantile(q.cnv, probs = 0.5, na.rm = T), by = c("pave.name", "batch.name", "albedo")][order(V1)]
 all.surface.data[, max(q.cnv, na.rm = T), by = c("pave.name", "batch.name", "albedo")][order(V1)]
 
-all.surface.data[, max(q.cnv, na.rm = T), by = c("day.time")][order(V1)]
+all.surface.data[, max(q.cnv, na.rm = T), by = c("daytime")][order(V1)]
+all.surface.data[, min(q.cnv, na.rm = T), by = c("daytime")][order(V1)]
 
+# IR
+all.surface.data[, max(q.rad, na.rm = T), by = c("daytime")][order(V1)]
+all.surface.data[, min(q.rad, na.rm = T), by = c("daytime")][order(V1)]
+
+# which max out.flux
+all.surface.data[,max(q.rad), by = c("pave.name", "batch.name", "albedo")][order(V1)]
